34ff93fb4f6c1b7dad31cb04192e1d99d90b0296
CharisHadi <charishadi@gmail.com>
2019-04-27 16:42:46 -0700

    only letter keys are recognized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,16 +22,9 @@ var startvalue = 0;
 function game_play(){
     document.getElementById("start").innerHTML= ""
     
-
-    var randomindex = Math.trunc((Math.random() * 100)%(words_list.length));
-
-
-    var word_chosen = words_list[randomindex];
-
     if(startvalue === 0){
-    document.getElementById("guess-word").innerHTML= words_list[randomindex];
+        randomize();
     };
-    var guesses = 
     
     guess();
 
@@ -41,9 +34,13 @@ function game_play(){
 
 
 function guess(){
+    if(event.keyCode >= 65 && event.keyCode <= 90){
         var letter = event.key.toLowerCase();
         var guessed_let = document.createTextNode(letter.toUpperCase());
         var guessed_list = document.getElementById("guessed-letters").innerHTML;
+
+        /*For loop to cycle through the Guessed Letters List to prevent replications
+        / and thus preventing wasted guesses. */
        // console.log("guessed_list length: " + guessed_list.length);
         for(i=-1 ; i < (guessed_list.length) ; i++){
            // console.log("For Loop: ", "guess is: " + letter.toUpperCase(), "letter in index: " + guessed_list[i], "i = " + i);
@@ -61,8 +58,14 @@ function guess(){
                 document.getElementById("guessed-letters").appendChild(guessed_let);
                 break;
             }
-        }   
+        }
+    }   
     //console.log("___________________________________")
-
-
 }
+
+function randomize(){
+    var randomindex = Math.trunc((Math.random() * 100)%(words_list.length));
+    var word_chosen = words_list[randomindex];
+    console.log(word_chosen);
+    document.getElementById("guess-word").innerHTML= words_list[randomindex];
+};
